1e962668619079e94db48b9dbd8512be326a005f
narblaitiin <regis.rousseau@insa-lyon.fr>
2025-01-23 15:49:17 +0100

    change code for environment sensor

--- solar_pi0_ws_abp/03-run/raspberry/lorawan_sensor.py
+++ solar_pi0_ws_abp/03-run/raspberry/lorawan_sensor.py
@@ -5,23 +5,38 @@
 # version 1.2 - 26/09/22 (ß)
 # version 1.3 - 12/12/22 (button off and poweroff the ssd1306 display after test)
 
-import adafruit_ssd1306, adafruit_bmp3xx, board, busio, time
+import adafruit_ssd1306, board, busio, time
+import BME280       # measuring temperature, humidity, and air pressure sensor
+import LTR390       # UV sensor
+import TSL2591      # digital ambient light sensor, for measuring IR and visible light
+import math
+import smbus
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
 from time import sleep
 from busio import I2C
 
-def getPayloadMockBMP388():
-    press_val = bmp.pressure
-    temp_val = bmp.temperature + temperature_offset
+def getPayloadMockSensor():
 
-    return encodePayload(press_val,temp_val)
+    bme = []
+    bme = bme280.readData()
+    press_val = round(bme[0], 2) 
+    temp_val = round(bme[1], 2) 
+    hum_val = round(bme[2], 2)
+        
+    lux_val = round(light.Lux(), 2)
+    uvs_val = uv.UVS()
+    
+    return encodePayload(press_val,temp_val,hum_val,lux_val,uvs_val)
 
-def encodePayload(pressure,temperature):
+def encodePayload(pressure,temperature,humidity,luxmen,uvs):
     # encode float as int
     press_val = int(pressure * 100) 
     temp_val = int(temperature * 100)
     abs_temp_val = abs(temp_val)
+    hum_val = int(humidity * 100)
+    lux_val = int(luxmen * 100)
+    uvs_val = int(uvs * 100)
 
     # encode payload as bytes
     # pressure (needs 2 bytes)
@@ -35,6 +50,18 @@ def encodePayload(pressure,temperature):
         data[2] = 0 & 0xff
     data[3] = (abs_temp_val >> 8) & 0xff
     data[4] = abs_temp_val & 0xff
+
+    # humidity (needs 2 bytes)
+    data[5] = (hum_val >> 8) & 0xff
+    data[6] = hum_val & 0xff
+
+    # luxmen (needs 2 bytes)
+    data[7] = (lux_val >> 8) & 0xff
+    data[8] = lux_val & 0xff
+
+    # uv (needs 2 bytes)
+    data[9] = (uvs_val >> 8) & 0xff
+    data[10] = uvs_val & 0xff
 
     return data
 
@@ -70,14 +97,13 @@ i2c = board.I2C()   # uses board.SCL and board.SDA
 reset_pin = DigitalInOut(board.D4)
 display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
 
-# create library object using our Bus I2C port
-bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
-
-# change this to match the location's pressure (hPa) at sea level
-bmp.sea_level_pressure = 1013.25
-bmp.pressure_oversampling = 1
-bmp.temperature_oversampling = 1
-temperature_offset = -5
+# create library object
+bus = smbus.SMBus(1)
+bme280 = BME280.BME280()
+bme280.get_calib_param()
+light = TSL2591.TSL2591()
+uv = LTR390.LTR390()
+sgp = SGP40.SGP40()
 
 # clear the display.
 display.fill(0)
@@ -86,9 +112,9 @@ width = display.width
 height = display.height
 
 # 5b array to store sensor data
-data = bytearray(5)
+data = bytearray(11)
 
-for meas in range (0, 3, 1):
+for meas in range (0, 10, 1):
     packet = None
     sendDataTTN(getPayloadMockBMP388())
     time.sleep(30)
